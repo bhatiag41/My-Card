57bf7e19d1764091b040ba78162e3e6dc27fb8b1
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2023-09-29 22:25:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function Main(){
         </div>
         <div className='content'>
             <h4>About</h4>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p>I am a frontend developer with a particular interest in making things cool and stylish. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
             <h4>Interests</h4>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p>Photography. Video-Editing. Internet fanatic. Anime. Travel geek. Pop culture ninja. Chai fanatic.</p>
         </div>
         <footer>
         
